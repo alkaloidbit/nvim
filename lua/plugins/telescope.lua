83799525808e31db47e1c1fa496536e890d66df8
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-10-23 17:13:31 +0200

    feat: add emoji-cmp source

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -406,6 +406,18 @@ return {
     end,
   },
 
+  -- add telescope-fzf-native
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+
   {
     "stevearc/aerial.nvim",
     config = true,
