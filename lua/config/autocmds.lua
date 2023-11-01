f6eaff9355ed8f9f9b06dd19288e4841f108db42
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-11-01 13:22:21 +0100

    feat: add LspCapabilities cmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,29 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_user_command("LspCapabilities", function()
+  local curBuf = vim.api.nvim_get_current_buf()
+  local clients = vim.lsp.get_active_clients({ bufnr = curBuf })
+
+  for _, client in pairs(clients) do
+    if client.name ~= "null-ls" then
+      local capAsList = {}
+      for key, value in pairs(client.server_capabilities) do
+        if value and key:find("Provider") then
+          local capability = key:gsub("Provider$", "")
+          table.insert(capAsList, "- " .. capability)
+        end
+      end
+      table.sort(capAsList) -- sorts alphabetically
+      local msg = "# " .. client.name .. "\n" .. table.concat(capAsList, "\n")
+      vim.notify(msg, "info", {
+        on_open = function(win)
+          local buf = vim.api.nvim_win_get_buf(win)
+          vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
+        end,
+        timeout = 14000,
+      })
+      vim.fn.setreg("+", "Capabilities = " .. vim.inspect(client.server_capabilities))
+    end
+  end
+end, {})
