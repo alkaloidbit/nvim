640e93ab0e620f1dde9db0ea2d9736824d07ff43
fredbadlieutenant <fredoudjoudi@gmail.com>
2024-02-11 00:48:08 +0100

    Update telescope keymaps <spce>uC = Telescope themes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -139,6 +139,11 @@ return {
         "<cmd>Telescope aerial<cr>",
         desc = "Goto Symbol (Aerial)",
       },
+      {
+        "<leader>uC",
+        "<cmd>Telescope themes<cr>",
+        desc = "Colorscheme themes",
+      },
       { "<localleader>r", "<cmd>Telescope resume initial_mode=normal<CR>", desc = "Resume last" },
       { "<localleader>R", "<cmd>Telescope pickers<CR>", desc = "Pickers" },
       { "<localleader>n", plugin_directories, desc = "Plugins" },
@@ -384,27 +389,27 @@ return {
       require("telescope").load_extension("luasnip")
 
       -- Highlights
-      local fg_bg = require("utils").fg_bg
-      local bg = require("utils").bg
-      local fg = require("utils").fg
-
-      local colors = require("colorscheme.nord.named_colors")
-
-      fg_bg("TelescopePromptPrefix", colors.red, colors.black2)
-
-      bg("TelescopeNormal", colors.darker_black)
-
-      fg_bg("TelescopePreviewTitle", colors.black, colors.green)
-
-      fg_bg("TelescopePromptTitle", colors.black, colors.red)
-
-      fg_bg("TelescopeSelection", colors.white, colors.black2)
-
-      fg_bg("TelescopeBorder", colors.darker_black, colors.darker_black)
-      fg_bg("TelescopePromptBorder", colors.black2, colors.black2)
-      fg_bg("TelescopePromptNormal", colors.white, colors.black2)
-      fg_bg("TelescopeResultsTitle", colors.darker_black, colors.darker_black)
-      fg_bg("TelescopePromptPrefix", colors.red, colors.black2)
+      -- local fg_bg = require("utils").fg_bg
+      -- local bg = require("utils").bg
+      -- local fg = require("utils").fg
+      --
+      -- local colors = require("colorscheme.nord.named_colors")
+      --
+      -- fg_bg("TelescopePromptPrefix", colors.red, colors.black2)
+      --
+      -- bg("TelescopeNormal", colors.darker_black)
+      --
+      -- fg_bg("TelescopePreviewTitle", colors.black, colors.green)
+      --
+      -- fg_bg("TelescopePromptTitle", colors.black, colors.red)
+      --
+      -- fg_bg("TelescopeSelection", colors.white, colors.black2)
+      --
+      -- fg_bg("TelescopeBorder", colors.darker_black, colors.darker_black)
+      -- fg_bg("TelescopePromptBorder", colors.black2, colors.black2)
+      -- fg_bg("TelescopePromptNormal", colors.white, colors.black2)
+      -- fg_bg("TelescopeResultsTitle", colors.darker_black, colors.darker_black)
+      -- fg_bg("TelescopePromptPrefix", colors.red, colors.black2)
     end,
   },
   {
